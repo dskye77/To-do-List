71fb16ecefcdee2c17afb893f2e7dc4b73b31a82
dskyle77 <dskyle77@gmail.com>
2025-09-01 16:35:17 +0100

    ToDoList

---
+++ local_storage/local_storage.js
@@ -0,0 +1,149 @@
+const ToDoList = {
+  List: [],
+  init() {
+    this.loadList()
+      .then(() => {
+        this.displayTasks(this.List);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    this.bindEvents();
+  },
+  bindEvents() {
+    document.getElementById("addTask").addEventListener("click", () => {
+      const input = document.getElementById("taskInput");
+      const inputValue = input.value;
+      this.createTask(inputValue, input);
+    });
+    document
+      .getElementById("tasks")
+      .addEventListener("click", (e) => this.handleTaskClicks(e));
+  },
+  createTask(name, input = null) {
+    if (!this.taskNameValidation(name)) return;
+    const task = {
+      title: name,
+      id: Date.now(),
+      isCompleted: false,
+    };
+    this.addTask(task);
+    this.displayTasks(this.List);
+    this.saveList();
+
+    if (input) input.value = ""; //reset task input
+  },
+  addTask(taskObj) {
+    this.List.push(taskObj);
+  },
+  taskNameValidation(name) {
+    //check empty input
+    if (name.trim() === "") {
+      window.alert("task title can't be empty");
+      return false;
+    }
+
+    //check already existing task
+    for (const task of this.List) {
+      if (task.title === name) {
+        window.alert("This task already exists");
+        return false;
+      }
+    }
+    return true;
+  },
+  displayTasks(taskArray = this.List) {
+    document.getElementById("tasks").innerHTML = "";
+    taskArray.sort((a, b) => a.isCompleted - b.isCompleted);
+    taskArray.forEach((task) => {
+      document
+        .getElementById("tasks")
+        .insertAdjacentHTML(
+          "beforeend",
+          this.taskHTML(task.title, task.id, task.isCompleted)
+        );
+    });
+  },
+  taskHTML(name, id, completed) {
+    return `<div class="task row ${
+      completed ? "completed" : ""
+    }" data-id="${id}">
+              <input class="check" type="checkbox" ${
+                completed ? "checked" : ""
+              }>
+              <p class="task-name">${name}</p>
+              <button class="delete-task" type="button">
+                <img src="/images/delete-button.svg" alt="" />
+              </button>
+              <button class="edit-task" type="button">
+                <img src="/images/edit-pencil.svg" alt="" srcset="" />
+              </button>
+            </div>`;
+  },
+  handleTaskClicks(e) {
+    const target = e.target;
+    const taskDiv = target.closest(".task");
+    let taskId = null;
+    if (taskDiv) {
+      taskId = parseInt(taskDiv.dataset.id);
+    }
+    if (target.classList.contains("delete-task")) {
+      this.deleteTask(taskId);
+    } else if (target.classList.contains("edit-task")) {
+      this.editTask(taskId);
+    } else if (target.classList.contains("check")) {
+      this.toggleCompleted(taskId);
+    }
+  },
+  deleteTask(id) {
+    let taskIndex = this.getIdx(id);
+    if (taskIndex === -1) return;
+
+    this.List.splice(taskIndex, 1);
+    this.displayTasks(this.List);
+    this.saveList();
+  },
+  editTask(id) {
+    let taskIndex = this.getIdx(id);
+    if (taskIndex === -1) return;
+    const title = this.List[taskIndex].title;
+    const newTitle = window.prompt(`Enter a new name for the task ${title}`);
+
+    if (!newTitle) return;
+    if (!this.taskNameValidation(newTitle)) return;
+
+    this.List[taskIndex].title = newTitle;
+    this.displayTasks(this.List);
+    this.saveList();
+  },
+  toggleCompleted(id) {
+    let taskIndex = this.getIdx(id);
+    if (taskIndex === -1) return;
+
+    const currentState = this.List[taskIndex].isCompleted;
+    this.List[taskIndex].isCompleted = !currentState;
+    this.displayTasks(this.List);
+    this.saveList();
+  },
+  getIdx(id) {
+    for (let i = 0; i < this.List.length; i++) {
+      const task = this.List[i];
+      if (task.id === id) return i;
+    }
+    return -1;
+  },
+  saveList() {
+    localStorage.setItem("__List__", JSON.stringify(this.List));
+  },
+  loadList() {
+    return new Promise((resolve) => {
+      const list = JSON.parse(localStorage.getItem("__List__"));
+      if (list) {
+        this.List = list;
+        resolve(list);
+      }
+    });
+  },
+};
+ToDoList.init();
